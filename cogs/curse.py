a746e8a27874cafbdeb255a13c667a9ddff9c99b
SenseiHaketon <sinisahamburg97@gmail.com>
2020-12-09 23:30:23 +0100

    Add support for multi word insults Fix curse check is case sensitive

--- cogs/curse.py
+++ cogs/curse.py
@@ -28,14 +28,15 @@ class Curse(commands.Cog):
         await ctx.send(f'{message}**{self.get_random_curse()}**')
 
     @commands.command()
-    async def addcurse(self, ctx, curse_to_add):
-        if(len(curse_to_add) > 0 and curse_to_add not in self.curses):
+    async def addcurse(self, ctx, *curse_to_add):
+        combined_curse = ' '.join(curse_to_add)
+        if(len(combined_curse) > 0 and combined_curse.casefold() not in (entry.casefold() for entry in self.curses)):
             with open("curses.txt", "a", encoding="utf-8") as file:
-                file.write(f'\n{curse_to_add}')
+                file.write(f'\n{combined_curse}')
                 self.load_curses()
-                await ctx.send(f"*{curse_to_add}* was added to my database!")
-        elif(curse_to_add in self.curses):
-            await ctx.send(f"*{curse_to_add}* already is in my database you **{self.get_random_curse()}**!")
+                await ctx.send(f"*{combined_curse}* was added to my database!")
+        elif(combined_curse in self.curses):
+            await ctx.send(f"*{combined_curse}* already is in my database you **{self.get_random_curse()}**!")
 
 
 def setup(client):
